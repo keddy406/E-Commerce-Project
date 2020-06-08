79a9482509c44ee2bf582070fecd2fc124a823a6
keddy406 <54757799+keddy406@users.noreply.github.com>
2020-06-08 14:01:51 +0800

    error page

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
 export default class Default extends Component {
-    render() {
+    render() 
+    {
+        console.log(this.props)
         return (
-            <div>
-                <h3>Page not found</h3>
-            </div>
+            <div className="container">
+               <div className="row">
+                   <div className="col mx-auto text-center text-title text-uppcase pt-5">
+                       <h1 className="display-3">404</h1>
+                       <h1>error</h1>
+                       <h2>Page not found</h2>
+                       <h3>this requested Url <span className="text-danger">{this.props.location.pathname}</span>was not found</h3>
+                   </div>
+               </div>
+                </div>
         )
     }
 }
